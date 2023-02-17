68a2a39d870c221be0818218f93280d7faf3890c
Vanessa Garcia <112432406+VanessaGVilla@users.noreply.github.com>
2023-02-17 10:44:29 -0800

    edits/commits

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -27,6 +27,39 @@ namespace COMP003A.Assignment5
             Console.WriteLine("**************************************************");
         }
 
+        /// <summary>
+        /// Prints the given string as a separator
+        /// </summary>
+        /// <param name="separator">The string to print as a separator</param>
+        static void PrintSeparator(string separator)
+        {
+            Console.WriteLine(separator);
+        }
 
+        /// <summary>
+        /// Builds an isosceles triangle using the given character and size
+        /// </summary>
+        /// <param name="inputCharacter">The character to use for building the triangle</param>
+        /// <param name="size">The size of the triangle</param>
+        static void IsoscelesTriangleBuilder(char inputCharacter, int size)
+        {
+            // increasing triangle
+            for (int i = 1; i <= size; i++)
+            {
+                string line = new string(inputCharacter, i);
+                Console.WriteLine(line);
+            }
+            // decreasing triangle
+            for (int i = size - 1; i >= 1; i--)
+            {
+                string line = new string(inputCharacter, i);
+                Console.WriteLine(line);
+            }
+        }
     }
 }
+
+
+
+
+        
