2fbfba2967ca2dc45c1a9465814d286e74248fa5
Vanessa Garcia <112432406+VanessaGVilla@users.noreply.github.com>
2023-02-17 09:34:49 -0800

    Add project files.

---
+++ COMP003A.Assignment5/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    * Author: Vanessa Garcia
+    * Course: COMP-003A
+    * Purpose: Methods and XML Comments 5
+    */
+
+namespace COMP003A.Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintSeparator();
+            PrintSeparator("Separator with string parameter");
+            IsoscelesTriangleBuilder('*', 5);
+        }
+
+
+
+    }
+}
